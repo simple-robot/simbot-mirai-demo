720b54c28fcfdc4b92fb68f7c31dcc462189cb47
ForteScarlet <ForteScarlet@163.com>
2021-03-18 21:57:43 +0800

    update comment

--- src/main/java/love/simbot/example/SimbotExampleApplication.java
+++ src/main/java/love/simbot/example/SimbotExampleApplication.java
@@ -6,15 +6,17 @@ import love.forte.simbot.core.SimbotApp;
 
 /**
  * simbot 启动类。
- *
+ * <p>
  * 此处的注解配置了两个配置文件：
  * <ul>
  *     <li>simbot.yml</li>
  *     <li>simbot-dev.yml</li>
  * </ul>
- * 其中，{@code simbot-dev.yml} 是一个测试环境的配置文件，只有当启动参数中存在 `--Sdev` 的时候才会被使用。
+ * 其中，{@code simbot-dev.yml} 是一个测试环境的配置文件，只有当启动参数中存在 {@code --Sdev} 的时候才会被使用。
  * 如果你不需要一些特殊的配置文件，那么可以直接使用 {@code @SimbotApplication}.
+ * <p>
  * 默认情况下，默认的配置文件名称为 {@code simbot.yml} 或 {@code simbot.properties}
+ *
  * @author ForteScarlet
  */
 @SimbotApplication({
